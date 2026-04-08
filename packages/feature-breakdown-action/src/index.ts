1b0591bd9bf5fd9e7e79a3ace01a177b642779bb
nahom <nahom.d54@gmail.com>
2026-04-08 21:21:06 +0300

    feat(breakdown-action): add feature package for task breakdown actions and API integration

---
+++ packages/feature-breakdown-action/src/index.ts
@@ -0,0 +1,10 @@
+export { BreakdownActionApiClient } from "./client.js";
+export { BreakdownButton } from "./BreakdownButton.js";
+
+export type {
+  BreakdownActionClientConfig,
+  BreakdownActionItem,
+  BreakdownActionRequest,
+  BreakdownActionResponse,
+} from "./types.js";
+export type { BreakdownButtonProps } from "./BreakdownButton.js";
