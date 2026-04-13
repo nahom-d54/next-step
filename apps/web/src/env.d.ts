cb9d5436f9b4eb56a688f585c9e4d32dd406513f
Milki7 <milkionline7@gmail.com>
2026-04-13 19:44:27 +0300

    fix(config): resolve project reference errors and enable composite builds

---
+++ apps/web/src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+declare module '*.css'
+declare module '*.scss'
+declare module '*.sass'
+declare module '*.less'
+declare module '*.styl'
+declare module '*.module.css'
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.webp'
